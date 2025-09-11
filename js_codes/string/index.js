375922efec8c85cd4fa4e8dee4bd9b095640c7b1
janvi7114 <chichuddejanvi@gmail.com>
2025-09-12 00:06:31 +0530

    learnt strings

--- js_codes/string/index.js
+++ js_codes/string/index.js
@@ -25,3 +25,24 @@ console.log(full_name);
 
 console.log(initial_name.length);//count space//
 console.log(initial_name.toUpperCase());
+
+let g="NAME";
+console.log(g.toLowerCase());
+
+console.log(g.substring(2));//at index 2//
+console.log(g.substring(0,3));//from 0 to 2//
+
+let sentence="This is an example sentence to split into words";
+let word=sentence.split(' ');//split on the basis of space//
+console.log(word);
+
+/*double inverted commans in a string is treated as sepration
+thus to add double inverted commans in the sentence we use
+backslack before and after the completion thus a sentence
+cannot be splited on the basis of backslash when backslash is
+used inside the value of a string*/
+let sentence2="This is sentence \"two\" ";
+console.log(sentence2.split(' '));
+
+/*to join words we use join(' ')*/
+console.log(word.join(','));
