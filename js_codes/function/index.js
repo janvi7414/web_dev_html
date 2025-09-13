671afe4f182a788b11e0460da34d16b382b1b3fc
janvi7114 <chichuddejanvi@gmail.com>
2025-09-13 22:28:28 +0530

    learnt three syntax for func declaration

--- js_codes/function/index.js
+++ js_codes/function/index.js
@@ -5,3 +5,26 @@ function call_name(name){
 }
 
 call_name("Janvi");
+
+function call_name2(first, second){
+    console.log(first,second);
+}
+call_name2("Janvi","chichudde");
+
+function sum(a,b,c){
+    let sum=a+b+c;
+    return sum;
+}
+console.log(sum(2,3,4));
+
+//other syntax to store func inside a var//
+let output = function(a,b){
+    return a/b;
+}
+console.log(output(1,3));
+
+//arrow function//
+let xpowery = (x,y) =>{
+    return x**y;
+}
+console.log(xpowery(2,4));
