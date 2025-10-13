d09cf54f00cec841fb9a52a60f93c91b3233ba76
janvi7114 <chichuddejanvi@gmail.com>
2025-10-13 19:32:48 +0530

    var hoisting and funct hoisting

---
+++ js_codes/hoisting/index.js
@@ -0,0 +1,23 @@
+/*hoisting:
+    1. variable hoisting: for var keyword variables
+        (func can be called before it's declaration
+        as it is hoisted before the calling due to hoisting
+        feature but here only declaration is hoisted thus
+        gives undefined value)
+    2. function hoisting: for function created only using
+        standard syntax as below. this feature hoists
+        the func decalration as well as operation before the
+        calling even if func is called before it's declaration
+
+        hoisting is not for let and const keywords*/
+
+//var hoisting will show undefined//
+console.log(name);
+var name="janvi";
+
+//function hoisting//
+call_name("Janu");
+function call_name(my_name)
+{
+    console.log(my_name);
+}
