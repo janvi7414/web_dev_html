411dacb40ff9fe7d6302f7382649a91278b98649
janvi7114 <chichuddejanvi@gmail.com>
2025-10-15 00:16:32 +0530

    functions on date and time

--- js_codes/math/index.js
+++ js_codes/math/index.js
@@ -6,3 +6,17 @@ console.log(Math.floor(1.3));
 console.log(Math.ceil(1.3));
 console.log(Math.abs(-2));
 console.log(Math.random());
+
+
+//date and time using Date()
+let curr=new Date();
+console.log(curr);
+
+let date=new Date('October 15 2025 07:12');
+//sun 0, mon 1, ...
+console.log(date);
+
+console.log(date.getDay());
+console.log(date.getFullYear());
+date.setFullYear(2000);
+console.log(date);
