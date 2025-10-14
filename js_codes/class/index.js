d9a7883a4c5fcf541572269c1dd99952b78bfe57
janvi7114 <chichuddejanvi@gmail.com>
2025-10-14 23:57:25 +0530

    function operations like default parameters

--- js_codes/class/index.js
+++ js_codes/class/index.js
@@ -1,5 +1,6 @@
 
 class human{
+    //to access object use this keyword
     //properties
     age=10;//public scoped
     #weight=50;//private scoped
@@ -21,7 +22,7 @@ class human{
     }
     walk()
     {
-        console.log("I am walking ",this.#weight);
+        console.log("I am walking ");
     }
 }
 
@@ -30,4 +31,47 @@ class human{
 let obj1= new human();
 console.log(obj1.age);
 obj1.breath();
-console.log(obj1.walk());
+console.log(obj1.fetch_weight);
+
+
+//constructor
+class car{
+    model= "Hyundai";
+    speed= 120;
+
+  constructor(new_model,new_speed)
+    {
+        this.model=new_model;
+        this.speed=new_speed;
+    }
+}
+
+//creating object
+let car1= new car("tata",100);
+console.log(car1);
+
+/* default paramters: default values are set to
+the paramters if not passed when obj created*/
+
+function say_name(name="Janvi")
+{
+    console.log(name);
+    //Janvi if no value passed else the valus itself
+}
+
+say_name();
+
+//an array can be passed as a parameter//
+function array_function(arr=["Janvi",21,null])
+{
+    console.log(arr);
+}
+array_function();
+array_function(undefined);
+array_function(null);
+
+//if null passed as parameter when obj created
+//and also a default value set other than null it will
+//take null
+//when undefined value set it will take default value set
+
