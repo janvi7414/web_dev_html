b01bc210b3b64935607079ce21b53a5089a11da1
janvi7114 <chichuddejanvi@gmail.com>
2025-10-15 00:06:16 +0530

    basic maths operations

---
+++ js_codes/math/index.js
@@ -0,0 +1,8 @@
+console.log(Math.PI);
+console.log(Math.max(10,1,2,4,0));
+console.log(Math.min(10,1,2,4,0));
+console.log(Math.round(1.3));
+console.log(Math.floor(1.3));
+console.log(Math.ceil(1.3));
+console.log(Math.abs(-2));
+console.log(Math.random());
